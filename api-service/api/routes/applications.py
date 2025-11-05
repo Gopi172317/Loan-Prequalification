e70ef6c526d8314ac530bf7fede9fe53fb306cfb
Gopinath R <gopinathr@Gopinaths-MacBook-Pro.local>
2025-11-05 14:47:45 +0530

    Docker compose.yaml

--- api-service/api/routes/applications.py
+++ api-service/api/routes/applications.py
@@ -46,7 +46,3 @@ async def get_application_status(application_id: str, db: Session = Depends(get_
     if not db_app:
         raise HTTPException(status_code=404, detail="Application not found")
     return ApplicationResponse(application_id=str(db_app.id), status=db_app.status)
-
-
-
-isudhfidskjno
